05c7f997e657b423e6b4f6e432a3803104669263
Boris Shminke <boris@shminke.ml>
2021-02-23 15:45:16 +0100

    increase coverage threshold

--- show_report.sh
+++ show_report.sh
@@ -6,7 +6,7 @@ pycodestyle --max-doc-length 160 --ignore E203,E501,W503 \
 	    ${PACKAGE_NAME} tests
 pylint --rcfile=.pylintrc ${PACKAGE_NAME} tests
 mypy --config-file mypy.ini ${PACKAGE_NAME} tests
-pytest --cov ${PACKAGE_NAME} --cov-report term-missing --cov-fail-under=33 \
+pytest --cov ${PACKAGE_NAME} --cov-report term-missing --cov-fail-under=99 \
        --junit-xml test-results/isabelle-client.xml \
        --doctest-modules ${PACKAGE_NAME}
 cloc ${PACKAGE_NAME}
