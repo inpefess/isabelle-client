43124f2db7f9c3ee5e68d83fc3a88c11eb6904ee
Boris Shminke <boris@shminke.ml>
2021-02-23 15:51:24 +0100

    fix bug in the documentation

--- show_report.sh
+++ show_report.sh
@@ -2,6 +2,9 @@
 
 set -e
 PACKAGE_NAME=isabelle_client
+cd doc
+make clean html
+cd ..
 pycodestyle --max-doc-length 160 --ignore E203,E501,W503 \
 	    ${PACKAGE_NAME} tests
 pylint --rcfile=.pylintrc ${PACKAGE_NAME} tests
