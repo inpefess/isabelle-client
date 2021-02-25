075ee7813d9a49c43bc16d5ceb051727237ec647
Boris Shminke <boris@shminke.ml>
2021-02-26 00:25:11 +0100

    stop writing to file

--- isabelle_client/isabelle_client.py
+++ isabelle_client/isabelle_client.py
@@ -13,11 +13,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import io
 import json
 import re
 import socket
-from typing import List, Optional, cast
+from typing import List, Optional
 
 from isabelle_client.utils import IsabelleResponse, get_final_message
 
@@ -46,24 +45,21 @@ class IsabelleClient:
     def execute_command(
         self,
         command: str,
-        log_filename: Optional[str] = None,
         asynchronous: bool = True,
     ) -> IsabelleResponse:
         """
         executes an (asynchronous) command and waits for results
 
-        >>> from unittest.mock import Mock, patch, mock_open
+        >>> from unittest.mock import Mock, patch
         >>> isabelle_client = IsabelleClient("test", "localhost", 1000, "test")
         >>> res = Mock(return_value=IsabelleResponse(
         ...    "FINISHED", '{"session_id": "session_id__42"}', 42
         ... ))
         >>> connect = Mock()
         >>> send = Mock()
-        >>> open_mock = mock_open()
         >>> with (
         ...     patch("socket.socket.connect", connect),
         ...     patch("socket.socket.send", send),
-        ...     patch("builtins.open", open_mock),
         ...     patch("isabelle_client.isabelle_client.get_final_message", res)
         ... ):
         ...    response = isabelle_client.execute_command("test", "test")
@@ -77,12 +73,8 @@ class IsabelleClient:
         [call(('localhost', 1000))]
         >>> print(send.mock_calls)
         [call(b'test\\ntest\\n')]
-        >>> print(open_mock.mock_calls)
-        [call('test', 'w'), call().close()]
 
         :param command: a full text of a command to ``isabelle``
-        :param log_filename: a file for saving a copy of all data received from
-            the server
         :param asynchronous: if ``False``, waits for ``OK``; else waits for
             ``FINISHED``
         :returns: ``isabelle`` server response
@@ -97,19 +89,10 @@ class IsabelleClient:
             tcp_socket.send(
                 f"{self.server_password}\n{command}\n".encode("utf-8")
             )
-            log_file = (
-                open(log_filename, "w") if log_filename is not None else None
-            )
-            response = get_final_message(
-                tcp_socket, final_message, cast(io.TextIOWrapper, log_file)
-            )
-            if log_file is not None:
-                log_file.close()
+            response = get_final_message(tcp_socket, final_message)
         return response
 
-    def session_start(
-        self, session_image: str = "HOL", log_filename: Optional[str] = None
-    ) -> str:
+    def session_start(self, session_image: str = "HOL") -> str:
         """
         start a new session
 
@@ -131,21 +114,15 @@ class IsabelleClient:
         ValueError: Unexpected response type: OK
 
         :param session: a name of a session image
-        :param log_filename: a file for saving a copy of all data received from
-            the server
         :returns: a ``session_id``
         """
         arguments = json.dumps({"session": session_image})
-        response = self.execute_command(
-            f"session_start {arguments}", log_filename
-        )
+        response = self.execute_command(f"session_start {arguments}")
         if response.response_type == "FINISHED":
             return json.loads(response.response_body)["session_id"]
         raise ValueError(f"Unexpected response type: {response.response_type}")
 
-    def session_stop(
-        self, session_id: str, log_filename: Optional[str] = None
-    ) -> IsabelleResponse:
+    def session_stop(self, session_id: str) -> IsabelleResponse:
         """
         stop session with given ID
 
@@ -159,19 +136,16 @@ class IsabelleClient:
         FAILED
 
         :param session_id: a string ID of a session
-        :param log_filename: a file for saving a copy of all data received from
-            the server
         :returns: ``isabelle`` server response
         """
         arguments = json.dumps({"session_id": session_id})
-        return self.execute_command(f"session_stop {arguments}", log_filename)
+        return self.execute_command(f"session_stop {arguments}")
 
     def use_theories(
         self,
         theories: List[str],
         session_id: Optional[str] = None,
         master_dir: Optional[str] = None,
-        log_filename: Optional[str] = None,
     ) -> IsabelleResponse:
         """
         run the engine on theory files
@@ -194,24 +168,21 @@ class IsabelleClient:
             created and then destroyed after trying to process theories
         :param master_dir: where to look for theory files; if ``None``, uses a
             temp folder of the session
-        :param log_filename: a file for a copy of all server messages
         :returns: ``isabelle`` server response
         """
         new_session_id = (
-            self.session_start(log_filename=log_filename)
-            if session_id is None
-            else session_id
+            self.session_start() if session_id is None else session_id
         )
         try:
             arguments = {"session_id": new_session_id, "theories": theories}
             if master_dir is not None:
                 arguments["master_dir"] = master_dir
             response = self.execute_command(
-                f"use_theories {json.dumps(arguments)}", log_filename
+                f"use_theories {json.dumps(arguments)}"
             )
         finally:
             if session_id is None:
-                self.session_stop(new_session_id, log_filename)
+                self.session_stop(new_session_id)
         return response
 
     def echo(self, message: str) -> IsabelleResponse:
