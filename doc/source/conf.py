f11bef17ad262b4b3d4984dd03aa820f695b621a
Boris Shminke <boris@shminke.ml>
2021-03-09 21:15:08 +0100

    boost version

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -34,7 +34,7 @@ copyright = "2021, Boris Shminke"
 author = "Boris Shminke"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.2"
+release = "0.1.3"
 
 
 # -- General configuration ---------------------------------------------------
