ef3c4c91aa05714c18e8596f41a76b982d63acdb
Boris Shminke <boris@shminke.ml>
2021-03-10 19:30:46 +0100

    check example quality

--- show_report.sh
+++ show_report.sh
@@ -6,9 +6,9 @@ cd doc
 make clean html
 cd ..
 pycodestyle --max-doc-length 160 --ignore E203,E501,W503 \
-	    ${PACKAGE_NAME} tests
-pylint --rcfile=.pylintrc ${PACKAGE_NAME} tests
-mypy --config-file mypy.ini ${PACKAGE_NAME} tests
+	    ${PACKAGE_NAME} tests examples
+pylint --rcfile=.pylintrc ${PACKAGE_NAME} tests examples
+mypy --config-file mypy.ini ${PACKAGE_NAME} tests examples
 pytest --cov ${PACKAGE_NAME} --cov-report term-missing --cov-fail-under=99 \
        --junit-xml test-results/isabelle-client.xml \
        --doctest-modules ${PACKAGE_NAME}
