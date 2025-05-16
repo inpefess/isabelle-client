2c15a27b408745158234ff4e7a6b6ecea29fcc23
Boris Shminke <boris@shminke.com>
2025-05-16 20:18:17 +0200

    use ruff instead of pydocstyle

--- doc/auto_examples/plot_example.py
+++ doc/auto_examples/plot_example.py
@@ -11,11 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# noqa: D205
+# pylint: disable-all
 """
 Basic usage example.
 ====================
-"""
+"""  # noqa: D205
 
 # %%
 # In what case to use
