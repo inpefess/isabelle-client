df80f4ec0f4a9495008e9ee84f6c9d99368e04fb
Boris Shminke <boris@shminke.com>
2025-05-16 20:19:16 +0200

    remove bandit

--- local-build.sh
+++ local-build.sh
@@ -11,7 +11,6 @@ pylint ${PACKAGE_NAME}
 mypy ${PACKAGE_NAME}
 find ${PACKAGE_NAME} -name "*.py" | xargs -I {} pyupgrade --py39-plus {}
 pyroma -n 10 .
-bandit -r ${PACKAGE_NAME}
 coverage run
 coverage report
 scc --no-cocomo --by-file -i py ${PACKAGE_NAME}
